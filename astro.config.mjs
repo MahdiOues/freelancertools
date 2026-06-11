4e2119aff5638dd714708faa7d1f3940d74e32f6
MahdiOues <reachmahdi@gmail.com>
2026-06-11 10:46:36 +0100

    sitemap updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://www.megalancer.online',
   output: 'static',
   adapter: vercel(),
+  integrations: [sitemap()],
 });
